7a67ff1a528bb389f92ba5e8509ecc5184243bc6
Andrea Onorato <andreino.ao@gmail.com>
2024-09-30 13:38:32 +0200

    fibonacci update

---
+++ main.py
@@ -0,0 +1,12 @@
+# Function to print the first 10 Fibonacci numbers
+def fibonacci(n):
+    fib_sequence = [0, 1]  # Start with the first two numbers in the Fibonacci sequence
+    while len(fib_sequence) < n:
+        next_number = fib_sequence[-1] + fib_sequence[-2]  # Sum of the last two numbers
+        fib_sequence.append(next_number)
+    
+    return fib_sequence
+
+# Print the first 10 Fibonacci numbers
+first_10_fibonacci = fibonacci(10)
+print(first_10_fibonacci)
